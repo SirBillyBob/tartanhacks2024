790a3bb0518a29380507dd01b4780d97c92d3d7f
Prasanna <ppantha28@gmail.com>
2024-02-02 22:16:12 -0500

    update mines.py

--- mines.py
+++ mines.py
@@ -5,13 +5,13 @@ from PIL import Image as img
 def onAppStart(app):
     app.width = 800
     app.height = 800
-    app.background = 'Green'
+    app.background = rgb(29, 94, 67)
     app.grid = createGrid(app)
-    app.prob = 10
+    app.prob = 5
     app.gameOver = False
+    app.clicked = 0
 
     # images
-    
     explosion = img.open('mines_assets/explosion.png')
     diamond = img.open('mines_assets/gems.png')
     app.diamondList = []
@@ -33,12 +33,15 @@ def onAppStart(app):
 
 
 def redrawAll(app):
+    drawLabel("Mines", app.width//2, app.height//10, align = 'center', font = 'monospace', size = 50, fill = 'white',  bold = True)
     drawGrid(app, app.grid)
 
 def onStep(app):
     app.gemImgIndex += 1
     if app.gameOver: 
         app.explosionImageIndex += 1
+    if app.clicked == 20:
+        app.gameOver = True
 
 def createGrid(app, x = 5 , y = 5):
     grid = []
@@ -53,7 +56,7 @@ def onMousePress(app, x, y):
     if not app.gameOver:
         if x > 150 and x < 650 and y > 150 and y < 650:
             currx = (x - 150)//100
-            curry = (y - 150)//100
+            curry = (y - 225)//100
             for i in app.grid:
                 for g in i:
                     if g.x == currx and g.y == curry:
@@ -67,17 +70,17 @@ def drawGrid(app, grid):
     for row in grid:
         for currGrid in row:
             x = 150 + currGrid.x*currGrid.width
-            y = 150 + currGrid.y*currGrid.height
+            y = 225 + currGrid.y*currGrid.height
             width = currGrid.width
             height = currGrid.height
             drawRect(x, y, width, height, fill = currGrid.color, border = currGrid.border)
     for row in grid:
         for currGrid in row:
             x = 150 + currGrid.x*currGrid.width
-            y = 150 + currGrid.y*currGrid.height
-            if currGrid.clicked and not app.gameOver:
+            y = 225 + currGrid.y*currGrid.height
+            if currGrid.clicked and ((not app.gameOver) or app.clicked == 20):
                 drawImage(app.diamondList[(app.gemImgIndex//4)%39], x+12.5, y+12.5)
-            elif currGrid.clicked and currGrid.mine:
+            elif currGrid.clicked and app.gameOver and not currGrid.mine:
                 if app.explosionImageIndex//2 < 25:
                     drawImage(app.explosionList[(app.explosionImageIndex//2)], x-50, y-50)
 
@@ -103,7 +106,8 @@ class Grid:
             self.color = 'red'
             self.app.gameOver = True
         else:
-            self.app.prob += 3
+            self.app.prob += 2
+            self.app.clicked += 1
             self.clicked = True
             self.mine = False
             self.color = 'lightSlateGrey'
